2e5f3e1c66d1121108e365b2891a62b788e46b7d
nooorulain <noorulannfatima@gmail.com>
2025-11-18 19:55:52 +0500

    basic setup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 
 export default function Home() {
   return (
     <div>
       <h1> Hello world</h1>
+      <ThemeToggle/>
     </div>
   );
 }
